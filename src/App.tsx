c7537ece3538931555085cdb8f2217e7cf6d93e5
qreedence <eden.yusof.ioannidis@gmail.com>
2025-01-29 10:57:10 +0100

    Install axios and TanStack Query, implement a test api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import TestRequests from "./components/TestRequests"
+
+const queryClient = new QueryClient()
+
 function App() {
   return (
-    <div className="bg-indigo-900 py-8 min-h-screen">
-      <div className="flex justify-center">
-        <p className="font-bold text-gray-50">
-          Navbar goes here
-        </p>
+    <QueryClientProvider client={queryClient}>
+      <div className="bg-indigo-900 py-8 min-h-screen">
+        <div className="flex justify-center">
+          <p className="font-bold text-gray-50">
+            Navbar goes here
+          </p>
+        </div>
+        <TestRequests/>
       </div>
-    </div>
-    )
-  };
+    </QueryClientProvider>
+  )};
 
 export default App
