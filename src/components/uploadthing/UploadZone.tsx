d49e52916196367bb6c7615de8f032b343d54a2b
qreedence <eden.yusof.ioannidis@gmail.com>
2025-02-13 13:55:42 +0100

    Remove pre-built UploadThing stuff

--- src/components/uploadthing/UploadZone.tsx
+++
@@ -1,4 +0,0 @@
-import { generateUploadDropzone } from "@uploadthing/react";
-import { UploadRouter } from "../../uploadthing/uploadthing";
-
-export const UploadDropzone = generateUploadDropzone<UploadRouter>();
