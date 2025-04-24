2bbcd4ea41dc3c8dc07353d975e2972300f2f369
qreedence <eden.yusof.ioannidis@gmail.com>
2025-04-24 16:10:26 +0200

    Import new game object structures

---
+++ src/apiclient/models/GameCoverDTO.ts
@@ -0,0 +1,9 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type GameCoverDTO = {
+    id?: number;
+    image_id?: string | null;
+};
+
